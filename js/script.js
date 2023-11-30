cda9d5a3ec3ec3fb173926c3faf5a5aefd0c8445
armaan-allidina <armaan-allidina>
2023-11-30 16:38:14 -0700

    j

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var a=false
+$("#box1").on("click", function(){
+    if(a==true){
+        a=false;
+    }
+    else{
+        a=true;
+    }
+});
+$("#box2").on("click", function(){
+    if(a==true){
+        a=false;
+    }
+    else{
+        a=true;
+    }
+});
+$("#box3").on("click", function(){
+    if(a==true){
+        a=false;
+    }
+    else{
+        a=true;
+    }
+});
+$("#box4").on("click", function(){
+    if(a==true){
+        a=false;
+    }
+    else{
+        a=true;
+    }
+});
+$("#box5").on("click", function(){
+    if(a==true){
+        a=false;
+    }
+    else{
+        a=true;
+    }
+});
