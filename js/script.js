dfbedbf805e1671468aafb7a97575d605fcca022
armaan-allidina <armaan-allidina>
2023-12-05 15:26:34 -0700

    f

--- js/script.js
+++ js/script.js
@@ -8,63 +8,63 @@ var j;
 var c=["#ff0000", "#00b400","#0000ff","#00ffff","#ff00ff","#ffff00","#000000","#ffffff"];
 $("#box1").on("click", function(){
     console.log('1');
-    if(a1==true){
-        $(this).css("transform", "translate(-100, -100)");
+     if(a1==true){
+        $(this).css("transform", "translate(-100px, -100px)");
         a1=false;
     }
     else{
-        $(this).css("transform", "translate(100, 100)");
+        $(this).css("transform", "translate(150px, 150px)");
         a1=true;
     }
 });
 $("#box2").on("click", function(){
     console.log('2');
     if(a2==true){
-        $(this).css("transform", "translate(0, -100)");
+        $(this).css("transform", "translate(0px, -100px)");
         a2=false;
     }
     else{
-        $(this).css("transform", "translate(0, 100)");
+        $(this).css("transform", "translate(0px, 100px)");
         a2=true;
     }
 });
 $("#box3").on("click", function(){
     if(a3==true){
-        $(this).css("transform", "translate(100, -100)");
+        $(this).css("transform", "translate(100px, -100px)");
         a3=false;
     }
     else{
-        $(this).css("transform", "translate(-100, 100)");
+        $(this).css("transform", "translate(-100px, 100px)");
         a3=true;
     }
 });
 $("#box4").on("click", function(){
     if(a4==true){
-        $(this).css("transform", "translate(0, 100)");
+        $(this).css("transform", "translate(0px, 100px)");
         a4=false;
     }
     else{
-        $(this).css("transform", "translate(0, -100)");
+        $(this).css("transform", "translate(0px, -100px)");
         a4=true;
     }
 });
 $("#box5").on("click", function(){
     if(a5==true){
-        $(this).css("transform", "translate(-100, 100)");
+        $(this).css("transform", "translate(-100px, 100px)");
         a5=false;
     }
     else{
-        $(this).css("transform", "translate(100, -100)");
+        $(this).css("transform", "translate(100px, -100px)");
         a5=true;
     }
 });
 $("#box6").on("click", function(){
     if(a6==true){
-        $(this).css("transform", "translate(100, 100)");
+        $(this).css("transform", "translate(100px, 100px)");
         a6=false;
     }
     else{
-        $(this).css("transform", "translate(-100, -100)");
+        $(this).css("transform", "translate(-100px, -100px)");
         a6=true;
     }
 });
